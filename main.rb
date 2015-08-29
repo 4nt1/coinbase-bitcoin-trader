c7bcf7d004ba632f7dddce39895f817af4127e2e
4nt1 <antoinemary@hotmail.fr>
2015-08-29 16:29:58 +0200

    send mail when orders given

--- main.rb
+++ main.rb
@@ -11,17 +11,13 @@ p "Balance : #{euros} EUR"
 
 if bitcoins > 0
   @euro_price = (@euro_price * 1.01).round(3)
-  p "Place order for selling #{bitcoins} BTC at #{@euro_price} EUR"
+  Mail.send("Place order for selling #{bitcoins} BTC at #{@euro_price} EUR")
   order = Api.sell(BigDecimal(bitcoins), @euro_price)
-
-  order   = Api.order(order.id)
-  @euro_price  = BigDecimal(order.funds.to_f / order.filled_size.to_f)
-  p "Sold #{bitcoins} BTC at #{@euro_price} EUR"
 
 elsif euros > 0
   euros = BigDecimal(Api.eur_account.balance).round(3)
   bitcoins = (euros / @euro_price * 0.9975).round(8)
-  p "Place order for buying #{bitcoins} bitcoins at #{@euro_price} EUR"
+  Mail.send("Place order for buying #{bitcoins} bitcoins at #{@euro_price} EUR")
   order = Api.buy(bitcoins, @euro_price)
 end
 
@@ -45,15 +41,18 @@ loop do
         when 'buy'
           (1..10).each do |nb|
             growth = 1 + nb/100.to_f
-            o = Api.sell(bitcoins/10, transaction.rate * growth)
+            euro_price = transaction.rate * growth
+            o = Api.sell(bitcoins/10, euro_price)
+            Mail.send("Place order for selling #{bitcoins/10} BTC at #{euro_price} EUR"
             Transaction.make_from_order(o)
           end
         when 'sell'
           obtained_euros = bitcoins * transaction.rate
           (1..10).each do |nb|
-            rate = transaction.rate * drop
-            btc_to_buy  = (obtained_euros / 10 / rate * 0.9975).round(8)
-            o = Api.buy(btc_to_buy, rate)
+            euro_price = transaction.rate * drop
+            btc_to_buy  = (obtained_euros / 10 / euro_price * 0.9975).round(8)
+            o = Api.buy(btc_to_buy, euro_price)
+            Mail.send("Place order for buying #{btc_to_buy} bitcoins at #{euro_price} EUR")
             Transaction.make_from_order(o)
           end
         end
